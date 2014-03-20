b10e08e642c892c6fd88f2b8060e8764272a1eac
Massimo Nocentini <massimo.nocentini@gmail.com>
2014-03-20 21:11:32 +0100

    Almost finished till exercise six, included.

---
+++ bezier.gnuplot
@@ -0,0 +1,25 @@
+set terminal postscript eps enhanced color font 'Helvetica,10'
+
+set output 'exercise-one.eps'
+plot [0:12] [0:10] "bezier-exercise-one.coordinates" with lines, "simple-control-polygon.dat" with linespoints pt 6
+
+set output 'exercise-two.eps'
+plot [0.5:3.5] [-0.3:1.3] "bezier-exercise-two.coordinates" with lines, "control-poly-exercise-two.coordinates" with linespoints pt 6
+
+set output 'exercise-four.eps'
+plot [-0.5:6.5] [-0.3:5] "control-poly-exercise-four.coordinates" with linespoints pt 6, \
+	"cyan-control-poly-exercise-four.coordinates" with linespoints pt 6, \
+	"magenta-control-poly-exercise-four.coordinates" with linespoints pt 6, \
+	"bezier-diagonal-exercise-four.coordinates" with lines, \
+	"bezier-last-exercise-four.coordinates" with lines
+
+
+set output 'exercise-six-original.eps'
+plot "bezier-original-exercise-six.coordinates" with lines, "control-poly-exercise-six.coordinates" with linespoints pt 6
+
+
+set output 'exercise-six-one-more-degree.eps'
+plot "bezier-one-more-degree-exercise-six.coordinates" with lines, "control-poly-exercise-six-one-more-degree.coordinates" with linespoints pt 6
+
+set output 'exercise-six-one-more-degree-comparison.eps'
+plot "bezier-one-more-degree-exercise-six.coordinates" with lines, "bezier-original-exercise-six.coordinates" with lines
--- deCasteljau.jl
+++ deCasteljau.jl
@@ -4,27 +4,124 @@ function drawCurve(aMatrix, aParameterVector)
 	x = zeros(n, 2)
 	for i = 1:n
 		param = aParameterVector[i]
-		aPoint = decasteljau(aMatrix, param)
+		aPoint, Diagonal, Last = decasteljau(aMatrix, param)
 		x[i,:] = aPoint
 	end
-	writedlm("simple.dat", x, '\t')
+	
+	x
 end
 
+function writeArrayForGnuplot(anArray, aFilename)
+	writedlm(aFilename, anArray, '\t')
+	anArray
+end
+
+function parametricCoordinates(xFn, yFn, pointsNumber)
+
+	parameterVector = linspace(0, 1, pointsNumber)	
+
+	coordinates = zeros(pointsNumber, 2)
+
+	for i = 1:pointsNumber
+		t = parameterVector[i]
+		coordinates[i,:] = [xFn(t) yFn(t)]		
+	end
+
+	coordinates
+
+end
 
 function decasteljau(controlPointsMatrix, t)
 	n = length(controlPointsMatrix[:,1])
 	Q = copy(controlPointsMatrix)
 
+	Diagonal = copy(controlPointsMatrix)
+	Last = copy(controlPointsMatrix)
+
+	Diagonal[1,:] = Q[1,:]
+	Last[1,:] = Q[n,:]
+	
 	for k = 1:n
 		for i = 1:n-k
 			Q_i = Q[i,:]
 			Q_isucc = Q[i+1,:]	
 			Q[i,:] = (1 - t)*Q_i + t*Q_isucc
 		end
+		
+		if k < n	
+			Diagonal[k+1,:] = Q[1,:]
+			Last[k+1,:] = Q[n-k,:]
+		end
 	end
-	return Q[1,:]
+	
+	Q[1,:], Diagonal, Last
+end
+
+function increaseDegree(originalControlPoints)
+	sandbox = [ [0 0]; copy(originalControlPoints); [0 0] ]
+	increasing = [copy(originalControlPoints); [0 0] ]
+	n = length(originalControlPoints[:,1])
+	
+	for i=1:n+1
+#		if i == 1
+#			increasing[1,:] = sandbox[2,:] 
+#		elseif i == n+1
+#			increasing[n+1,:] = sandbox[n+1,:] 
+#		else
+			ratio = (i-1)/(n)
+			increasing[i,:] = ratio*sandbox[i,:] + (1 - ratio)*sandbox[i+1,:] 
+#		end
+	end
+
+	increasing
 end
 
 function readSimplePolygon()
 	 readdlm("simple-control-polygon.dat")
+end
+
+function exercise_one ()
+	controlPoints = readSimplePolygon()
+	
+	bezierCurvePoints = drawCurve(controlPoints,
+					linspace(0,1,200))
+
+	writeArrayForGnuplot(bezierCurvePoints, "bezier-exercise-one.coordinates")
+end
+
+function exercise_two ()
+	controlPoints = parametricCoordinates(x -> 1 + x + x^2, y -> y^3, 6)
+
+	bezierCurvePoints = drawCurve(controlPoints,
+					linspace(0,1,200))
+
+	writeArrayForGnuplot(bezierCurvePoints, "bezier-exercise-two.coordinates")
+	writeArrayForGnuplot(controlPoints, "control-poly-exercise-two.coordinates")
+end
+
+function exercise_four ()
+	outerControlPoints = readdlm("control-poly-exercise-four.coordinates")
+	t_star = .25
+	q_value, Diagonal, Last = decasteljau(outerControlPoints, t_star)
+	writeArrayForGnuplot(Diagonal, "cyan-control-poly-exercise-four.coordinates")
+	writeArrayForGnuplot(Last, "magenta-control-poly-exercise-four.coordinates")
+
+	#diagonalBezierCurvePoints = drawCurve(Diagonal, [t/t_star for t in linspace(0,1,100)])
+	diagonalBezierCurvePoints = drawCurve(Diagonal, linspace(0,1,100))
+	lastBezierCurvePoints = drawCurve(Last, linspace(0,1,100))
+	writeArrayForGnuplot(diagonalBezierCurvePoints, "bezier-diagonal-exercise-four.coordinates")
+	writeArrayForGnuplot(lastBezierCurvePoints, "bezier-last-exercise-four.coordinates")
+end
+
+function exercise_six()
+	originalControlPoints = readdlm("control-poly-exercise-six.coordinates")
+	originalBezierCurvePoints = drawCurve(originalControlPoints, linspace(0,1,200))
+	writeArrayForGnuplot(originalBezierCurvePoints, "bezier-original-exercise-six.coordinates")
+
+	oneMoreDegreeControlPoints = increaseDegree(originalControlPoints)
+	oneMoreDegreeBezierCurvePoints = drawCurve(oneMoreDegreeControlPoints, linspace(0,1,200))
+	writeArrayForGnuplot(oneMoreDegreeControlPoints, "control-poly-exercise-six-one-more-degree.coordinates")
+	writeArrayForGnuplot(oneMoreDegreeBezierCurvePoints, "bezier-one-more-degree-exercise-six.coordinates")
+
+
 end
