7cf21ed642af6b86193f0e29545384c08c68ddc2
Massimo Nocentini <massimo.nocentini@gmail.com>
2014-12-24 23:08:29 +0100

    VERY BIG WIN! Surface subdivision works for triangular Bezier patches ^^

--- surfaces/trianglesDrawing.py
+++ surfaces/trianglesDrawing.py
@@ -3,13 +3,18 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def draw(surface, triangles, figure_size_tuple=(15,15)):
+def draw(*surfaces, figure_size_tuple=(15,15)):
 
     sizex, sizey = figure_size_tuple
     matplotlib.rcParams['figure.figsize'] = [sizex, sizey]
 
-    x, y, z = surface[0,:],surface[1,:],surface[2,:]
+    if len(surfaces) is 2:
+        surface, triangles = surfaces
+        surfaces = [(surface, triangles)]
 
     fig = plt.figure() 
     ax = fig.add_subplot(1, 1, 1, projection='3d') 
-    ax.plot_trisurf(x, y, z, triangles=triangles, cmap=plt.cm.Spectral)#, edgecolor='none')
+
+    for surface, triangles in surfaces:
+        x, y, z = surface[0,:],surface[1,:],surface[2,:]
+        ax.plot_trisurf(x, y, z, triangles=triangles, cmap=plt.cm.Spectral)#, edgecolor='none')
